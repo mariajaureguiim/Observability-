85a0a2c51e4f0d16fce1cdc262040bee6499aa80
mariajaureguiim <92944875+mariajaureguiim@users.noreply.github.com>
2023-11-26 13:24:08 -0500

    Update tracing.js

--- tracing.js
+++ tracing.js
@@ -1,31 +1,45 @@
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
-const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { trace } = require("@opentelemetry/api");
-//Instrumentations
+
+// Instrumentations
 const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
 const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
-//Exporter
+
+// Exporter
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
+
 module.exports = (serviceName) => {
-   const exporter = new ConsoleSpanExporter();
-   const provider = new NodeTracerProvider({
-       resource: new Resource({
-           [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-       }),
-   });
-   provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
-   provider.register();
-   registerInstrumentations({
-       instrumentations: [
-           new HttpInstrumentation(),
-           new ExpressInstrumentation(),
-           new MongoDBInstrumentation(),
-       ],
-       tracerProvider: provider,
-   });
-   return trace.getTracer(serviceName);
+  // Configure Jaeger Exporter
+  const jaegerExporter = new JaegerExporter({
+    serviceName: serviceName,
+  });
+
+  // Create a NodeTracerProvider with Jaeger Exporter
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    }),
+  });
+
+  // Add Jaeger Exporter as a span processor
+  provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
+
+  // Register the provider and instrumentations
+  provider.register();
+  registerInstrumentations({
+    instrumentations: [
+      new HttpInstrumentation(),
+      new ExpressInstrumentation(),
+      new MongoDBInstrumentation(),
+    ],
+    tracerProvider: provider,
+  });
+
+  // Return the tracer
+  return trace.getTracer(serviceName);
 };
